8e2c6b5bbbd8306c5f41f063e7cee0518d9364b3
KABOTE LUPAYA Destin <84081391+Destin-Lupaya@users.noreply.github.com>
2023-04-19 16:53:15 +0200

    Controller, Models and Seeders

---
+++ app/Http/Controllers/TerritoireController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Territoire;
+use App\Http\Requests\StoreTerritoireRequest;
+use App\Http\Requests\UpdateTerritoireRequest;
+
+class TerritoireController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTerritoireRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTerritoireRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Territoire  $territoire
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Territoire $territoire)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Territoire  $territoire
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Territoire $territoire)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTerritoireRequest  $request
+     * @param  \App\Models\Territoire  $territoire
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTerritoireRequest $request, Territoire $territoire)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Territoire  $territoire
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Territoire $territoire)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/UtilisateurController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Utilisateur;
+use App\Http\Requests\StoreUtilisateurRequest;
+use App\Http\Requests\UpdateUtilisateurRequest;
+
+class UtilisateurController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreUtilisateurRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUtilisateurRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Utilisateur  $utilisateur
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Utilisateur $utilisateur)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Utilisateur  $utilisateur
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Utilisateur $utilisateur)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateUtilisateurRequest  $request
+     * @param  \App\Models\Utilisateur  $utilisateur
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateUtilisateurRequest $request, Utilisateur $utilisateur)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Utilisateur  $utilisateur
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Utilisateur $utilisateur)
+    {
+        //
+    }
+}
