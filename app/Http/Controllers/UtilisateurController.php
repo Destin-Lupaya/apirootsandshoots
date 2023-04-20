2d79c5a8fef0a2cf068f7cded4eed37ed402a04c
KABOTE LUPAYA Destin <84081391+Destin-Lupaya@users.noreply.github.com>
2023-04-20 17:43:46 +0200

    User fonction login

--- app/Http/Controllers/UtilisateurController.php
+++ app/Http/Controllers/UtilisateurController.php
@@ -91,5 +91,53 @@ class UtilisateurController extends Controller
         //
     }
 
-    
+    public function destroy($id)
+    {
+        //
+    }
+
+    /**
+     * look for a specific resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        $utilisateurForm = (array) $request->all();
+
+        $email = isset($utilisateurForm['email']) ? $utilisateurForm['email'] : '';
+        $psw = isset($utilisateurForm['psw']) ? $utilisateurForm['psw'] : '';
+
+        $utilisateur = Users::where('email', '=', $email)->orwhere('telephone', '=', $email)->get();
+
+        $status = "404";
+
+        if (count($utilisateur) > 0) {
+
+            if ($utilisateur[0]['psw'] == $psw) {
+                if ($utilisateur[0]['statusActive'] == 0) {
+                    return response()->json(["message" => "Votre compte a été désactivé, veuillez contacter votre administrateur"], 403);
+                }
+
+                $status = "200";
+                $utilisateur = $utilisateur[0];
+                $account = Utilisateur::find($utilisateur['id'])->getAccount;
+
+                $authToken = $utilisateur->createToken('auth-token')->plainTextToken;
+
+                $response = ['utilisateur' => $utilisateur, 'account' => $account, 'access-token' => $authToken];
+            } else {
+
+                $response = ["message" => 'wrong password'];
+            }
+        } else {
+
+            $response = ["message" => 'wrong email ou telephone'];
+        }
+
+        return response()->json($response, $status);
+    }
+
+
 }
