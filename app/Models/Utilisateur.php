6fc85d007900b4242525c34d2995bbcac03ad12a
KABOTE LUPAYA Destin <84081391+Destin-Lupaya@users.noreply.github.com>
2023-04-20 17:49:23 +0200

    User Model

--- app/Models/Utilisateur.php
+++ app/Models/Utilisateur.php
@@ -5,7 +5,36 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Utilisateur extends Model
+class Utilisateur  extends Authenticatable
+//extends Model
 {
-    use HasFactory;
+    //use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+    protected $fillable = [
+        'names',
+        'email',
+        'telephone',
+        'username',
+        'psw',
+        'role',
+        'code',
+        'pin',
+        'avatar',
+        'statusActive'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+
+    function getAccount(){
+
+        return $this->hasMany('App\Models\Account');
+    }
 }
